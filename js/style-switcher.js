00816e618920d8cf7b379ec869ae73f17d7eb909
TUYISHIMIRE Hilbert <hilbert.apply@gmail.com>
2023-12-11 09:44:16 -0800

    added

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -23,7 +23,7 @@ const altStyle=document.querySelectorAll(".alt-style");
  const dayNight=document.querySelector(".day-night");
  dayNight.addEventListener('click',()=>{
     dayNight.querySelector('i').classList.toggle('fa-sun');
-    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
     document.body.classList.toggle('dark');
  })
  window.addEventListener('load',()=>{
@@ -31,6 +31,6 @@ const altStyle=document.querySelectorAll(".alt-style");
         dayNight.querySelector('i').classList.add('fa-sun');
     }
     else{
-        dayNight.querySelector('i').classList.add('fa-sun');
+        dayNight.querySelector('i').classList.add('fa-moon');
     }
  })
